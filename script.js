79ab199fb7ed7e56089fc2ae57ea0c88e278fcaa
Bruna Campos <brunareginadecampos@gmail.com>
2024-10-02 22:13:50 -0300

    Finish by Bruna

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function toggleMode() {
     // substituir a imagem
     if(html.classList.contains('light')){
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/white.png")
   } else{ 
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/dark.png")
     }
     
 }
